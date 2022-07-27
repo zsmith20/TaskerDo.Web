e9c38f28e88991fc3399e451eefc21a4c10da05d
Jacob Smith <Jacob_Smith@MechanicsBank.com>
2022-07-26 17:48:12 -0700

    Got it to build and deploy to Azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   ENVIRONMENT: 'PROD',
   api: {
-    app: 'https://localhost:44308/api',
+    app: 'https://taskerdo-web.azurewebsites.net/api',
   },
 };
